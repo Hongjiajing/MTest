bc9aa081f811d774481a1fc1d0b50c879b48313e
HJJ <hahahaxiansheng@163.com>
2019-11-10 09:22:08 +0800

    我的IDEA第三次修改

---
+++ src/main/java/MyTestClass.java
@@ -0,0 +1,10 @@
+/**
+ * @Description:
+ * @Author: Hong JiaJing
+ * @CreateDate: 2019.11.10 上午 9:18
+ **/
+public class MyTestClass {
+    public static void main(String[] args) {
+        System.out.println("我的git项目第二次测试");
+    }
+}
